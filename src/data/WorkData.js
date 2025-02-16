be89ec5df4085b15c6c084ed8a8d013a2f7c7456
Vikash Kumar Gupta <120501092+KrVikashGupta@users.noreply.github.com>
2025-02-16 18:32:34 +0530

    Add files via upload

---
+++ src/data/WorkData.js
@@ -0,0 +1,64 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Blogging Platform ",
+        description:"This repository contains the code for a Blogging Platform built using JavaScript and Node.js. It allows users to create, edit, and manage blog posts efficiently.",
+        tags:["Node.js","JavaScript","Express.js","MongoDB"],
+        
+        
+        demo:"https://github.com/KrVikashGupta",
+        github:"https://github.com/KrVikashGupta"
+    },
+    {
+        id:2,
+        name:"Job listing website",
+        description:"A Job listing website build in ReactJS. With searching and filtering functionalities.",
+        tags:["react","sass"],
+        
+        demo:"https://github.com/KrVikashGupta",
+        github:"https://github.com/KrVikashGupta"
+        
+    },
+    {
+        id:3,
+        name:"Dev.to Clone",
+        description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
+        tags:["react","api","sass"],
+        
+        demo:"https://github.com/KrVikashGupta",
+        github:"https://github.com/KrVikashGupta"
+    },
+    {
+        id:4,
+        name:"Sidebar Menu",
+        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"https://github.com/KrVikashGupta",
+        github:"https://github.com/KrVikashGupta"
+       
+    },{
+        id:5,
+        name:"Todo App(React + Redux)",
+        description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+        tags:["react","redux","framer-motion"],
+        
+        demo:"https://github.com/KrVikashGupta",
+        github:"https://github.com/KrVikashGupta"
+        
+    },
+    {
+        id:6,
+        name:"Responsive Navigation Menu",
+        description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+        tags:["react","react-router","css"],
+        
+        demo:"https://github.com/KrVikashGupta",
+        github:"https://github.com/KrVikashGupta"
+        
+    },
+    
+  
+] 
